822762f6d757a6e2189c027349d606934fdfc44d
John <codebeast0420@gmail.com>
2023-09-01 10:20:57 -0400

    connect with wallet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import PageButton from './components/PageButton';
+import ConnectButton from './components/ConnectButton';
+import { GearFill } from 'react-bootstrap-icons';
+import ConfigModal from './components/ConfigModal';
 const ethers = require("ethers");
 
 function App() {
   const [provider, setProvider] = useState(undefined);
   const [signer, setSigner] = useState(undefined);
   const [signerAddress, setSingerAddress] = useState(undefined);
+
+  const [showModal, setShowModal] = useState(undefined);
+  const [slippageAmount, setSlippageAmount] = useState(2);
+  const [deadlineMinutes, setDeadlineMinutes] = useState(10)
 
   useEffect(() => {
     const onLoad = async () => {
@@ -23,23 +31,56 @@ function App() {
 
   const isConnected = () => signer !== undefined;
   const getWalletAddress = () => {
-    signer.getAddress()
-    .then(address => {
-      setSingerAddress(address)
-
-      // todo: connect weth and uni contracts
-    })
+    signer.then((res) => setSingerAddress(res.address));
   }
 
 
-  if(signer !== undefined) {
+  if (signer !== undefined) {
     getWalletAddress();
   }
 
   return (
     <div className="App">
       <div className='appNav'>
-        <div className='my-2 buttonContainer buttonContainerTop'></div>
+        <div className='my-2 buttonContainer buttonContainerTop'>
+          <PageButton name={"Swap"} isBold={true} />
+          <PageButton name={"Pool"} />
+          <PageButton name={"Vote"} />
+          <PageButton name={"Charts"} />
+        </div>
+        <div className='rightNav'>
+          <div className='connectButtonContainer'>
+            <ConnectButton
+              provider={provider}
+              isConnected={isConnected}
+              signerAddress={signerAddress}
+              getSigner={getSigner}
+            />
+          </div>
+          <div className='my-2 buttonContainer'>
+            <PageButton name={"..."} />
+          </div>
+        </div>
+      </div>
+
+      <div className='appBody'>
+        <div className='swapContainer'>
+          <div className='swapHeader'>
+            <span className='swapText'>Swap</span>
+            <span className='gearContaner' onClick={() => setShowModal(true)}>
+              <GearFill />
+            </span>
+            {showModal && (
+              <ConfigModal
+                onClick={() => setShowModal(false)}
+                setDeadlineMinutes={setDeadlineMinutes}
+                deadlineMinutes={deadlineMinutes}
+                setSlippageAmount={setSlippageAmount}
+                slippageAmount={slippageAmount}
+              />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
