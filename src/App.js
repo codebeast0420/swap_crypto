a462927dc3f92ddf7cb3c323d327af33d49d5435
John <codebeast0420@gmail.com>
2023-09-01 05:56:24 -0700

    add provider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+const ethers = require("ethers");
 
 function App() {
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [signerAddress, setSingerAddress] = useState(undefined);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.BrowserProvider(window.ethereum);
+      setProvider(provider);
+    }
+    onLoad()
+  }, [])
+
+  const getSigner = async provider => {
+    provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    setSigner(signer);
+  }
+
+  const isConnected = () => signer !== undefined;
+  const getWalletAddress = () => {
+    signer.getAddress()
+    .then(address => {
+      setSingerAddress(address)
+
+      // todo: connect weth and uni contracts
+    })
+  }
+
+
+  if(signer !== undefined) {
+    getWalletAddress();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='appNav'>
+        <div className='my-2 buttonContainer buttonContainerTop'></div>
+      </div>
     </div>
   );
 }
